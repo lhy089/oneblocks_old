c7408d6490f8b5943f7c85b140210a0a58422733
lhy089 <hyong089@gmail.com>
2024-04-22 09:32:35 +0900

    [config] 최초 커밋

---
+++ src/main/java/com/oneblocks/OneblocksApplication.java
@@ -0,0 +1,13 @@
+package com.oneblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneblocks/OneblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
